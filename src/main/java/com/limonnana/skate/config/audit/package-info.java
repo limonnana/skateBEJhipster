75cfc6f05d0aec721b124e0f902222e6ede763af
limonnana <rosenzvaig@gmail.com>
2021-01-19 12:32:27 +0200

    Initial version of skate03 generated by JHipster-6.10.5

---
+++ src/main/java/com/limonnana/skate/config/audit/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Audit specific code.
+ */
+package com.limonnana.skate.config.audit;
---
+++ src/main/java/com/limonnana/skate/config/dbmigrations/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * MongoDB database migrations using MongoBee.
+ */
+package com.limonnana.skate.config.dbmigrations;
